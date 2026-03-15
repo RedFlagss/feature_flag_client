244ae39bd25108394454a4a91b6cc926e04d684f
SemionMur <simeon.muravyov@gmail.com>
2026-03-15 14:43:28 +0300

    added new module for client app with samples of lib use

---
+++ client-sample-spring/src/main/java/org/redflag/clientsamplespring/ClientSampleSpringApplication.java
@@ -0,0 +1,13 @@
+package org.redflag.clientsamplespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSampleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSampleSpringApplication.class, args);
+	}
+
+}
---
+++ client-sample-spring/src/test/java/org/redflag/clientsamplespring/ClientSampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redflag.clientsamplespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
