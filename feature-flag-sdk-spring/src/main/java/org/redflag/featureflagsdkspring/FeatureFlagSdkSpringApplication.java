100e3cd149e41c63217ac0efc03519cd4786e0ab
SemionMur <simeon.muravyov@gmail.com>
2026-03-15 14:32:58 +0300

    init

---
+++ feature-flag-sdk-spring/src/main/java/org/redflag/featureflagsdkspring/FeatureFlagSdkSpringApplication.java
@@ -0,0 +1,13 @@
+package org.redflag.featureflagsdkspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagSdkSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureFlagSdkSpringApplication.class, args);
+    }
+
+}
---
+++ feature-flag-sdk-spring/src/test/java/org/redflag/featureflagsdkspring/FeatureFlagSdkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redflag.featureflagsdkspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagSdkSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
