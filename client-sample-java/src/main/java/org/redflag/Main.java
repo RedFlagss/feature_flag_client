cabb21dcf63ce539d83c6cde94aa8247f3445617
SemionMur <simeon.muravyov@gmail.com>
2026-04-09 20:44:36 +0300

    finished sdk

--- client-sample-java/src/main/java/org/redflag/Main.java
+++ client-sample-java/src/main/java/org/redflag/Main.java
@@ -5,25 +5,26 @@ import org.redflag.server.DataStorage;
 import org.redflag.service.FeatureFlagService;
 import org.redflag.service.impl.FeatureFlagServiceImp;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
-        AppConfig appConfig = AppConfig.createInstance(
-                        "/",
-                        "login",
-                        "password"
-        );
+        AppConfig appConfig = AppConfig.getInstance();
         DataStorage dataStorage = DataStorage.getInstance(appConfig);
 
         FeatureFlagService featureFlagService = new FeatureFlagServiceImp(dataStorage);
 
-        if (featureFlagService.isEnabled("flag")) {
-            System.out.println("Feature Flag is enabled");
+        while (true) {
+            if (featureFlagService.isEnabled("flag")) {
+                System.out.println("Feature Flag is enabled");
+            }  else {
+                System.out.println("Feature Flag is disabled");
+            }
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
-        if (!featureFlagService.isEnabled("flag2")) {
-            System.out.println("Feature Flag 2 is disabled");
-        }
-
-        System.out.println("FF3 " + featureFlagService.isEnabled("ff3"));
     }
 }
--- feature-flag-sdk-core/src/main/java/org/redflag/server/DataStorage.java
+++ feature-flag-sdk-core/src/main/java/org/redflag/server/DataStorage.java
@@ -1,6 +1,9 @@
 package org.redflag.server;
 
 import org.redflag.configuration.AppConfig;
+import org.redflag.dto.CreateFeatureFlagRequest;
+import org.redflag.dto.FeatureFlagDTO;
+import org.redflag.dto.FeatureFlagsResponse;
 import org.redflag.service.ServerRequestsService;
 import org.redflag.service.impl.KafkaFeatureFlagConsumer;
 import org.redflag.service.impl.ServerRequestsServiceImp;
@@ -8,14 +11,16 @@ import org.redflag.service.impl.ServerRequestsServiceImp;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 public class DataStorage {
     private final Map<String, Boolean> ffStorage;
-    private AppConfig appConfig;
 
     private static DataStorage instance;
+
+    private AppConfig appConfig;
 
     private final ServerRequestsService serverRequestsService;
     private final KafkaFeatureFlagConsumer kafkaConsumer;
@@ -29,11 +34,10 @@ public class DataStorage {
     }
 
     private DataStorage(AppConfig appConfig) {
-        this.ffStorage = collectFlags();
         this.appConfig = appConfig;
-        this.serverRequestsService = new ServerRequestsServiceImp();
+        this.serverRequestsService = new ServerRequestsServiceImp(appConfig.getUsername());
+        this.ffStorage = serverRequestsService.getAllFeatureFlags();
         this.kafkaConsumer = new KafkaFeatureFlagConsumer(appConfig);
-
         this.kafkaConsumer.startConsuming();
 
         Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
@@ -43,39 +47,11 @@ public class DataStorage {
         Boolean kafkaValue = kafkaConsumer.getFlagValue(flagKey);
         if (ffStorage.putIfAbsent(flagKey, false) == null) {
             System.out.println("Flag not found: " + flagKey + " Creating flag.");
-            serverRequestsService.createFeatureFlag();
+            serverRequestsService.createFeatureFlag(new CreateFeatureFlagRequest(flagKey, false));
         };
 
         ffStorage.put(flagKey, kafkaValue);
         return ffStorage.get(flagKey);
-    }
-
-    private HashMap<String, Boolean> collectFlags() {
-        //TODO: replace with call to kafka topic / or add call to kafka topic
-
-        HashMap<String, Boolean> ffMap = new HashMap<>();
-
-        try (InputStream input = getClass().getClassLoader()
-                .getResourceAsStream("feature-flags.properties")) {
-
-            if (input == null) {
-                System.err.println("feature-flags.properties not found");
-                return null;
-            }
-
-            Properties props = new Properties();
-            props.load(input);
-
-            for (String key : props.stringPropertyNames()) {
-                boolean value = Boolean.parseBoolean(props.getProperty(key).trim());
-                ffMap.put(key, value);
-            }
-
-        } catch (IOException e) {
-            throw  new RuntimeException("Error occurred in feature-flags.properties", e);
-        }
-
-        return ffMap;
     }
 
     private void shutdown() {
